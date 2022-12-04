490b6c96f0ae3cc61e33cc158761ff5540833361
Muhammad Arham Jamil <MUHAMMADARHAMJAMILSABIH@gmail.com>
2022-12-04 15:48:05 +0500

    Updates

---
+++ precache-manifest.38a6fdb2658c1a5e5fc85104744535c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ca9b9ba9c08fd3412d7",
+    "url": "/ReactJs_TextAnalyserApp/static/css/main.8d1e6f53.chunk.css"
+  },
+  {
+    "revision": "0ca9b9ba9c08fd3412d7",
+    "url": "/ReactJs_TextAnalyserApp/static/js/main.0ca9b9ba.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/ReactJs_TextAnalyserApp/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/ReactJs_TextAnalyserApp/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "69d329894d0e18cb28cd",
+    "url": "/ReactJs_TextAnalyserApp/static/js/runtime~main.69d32989.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ReactJs_TextAnalyserApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "392ce018871c57ed6dfe458ba5544720",
+    "url": "/ReactJs_TextAnalyserApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ReactJs_TextAnalyserApp/precache-manifest.38a6fdb2658c1a5e5fc85104744535c7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ReactJs_TextAnalyserApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
